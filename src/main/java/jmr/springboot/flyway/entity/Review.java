ba4192aa5c8a99ec793c69bfeeb50c2fab063ebf
Jean-Marc Robin <jmr33bx@yahoo.fr>
2017-11-07 21:08:03 +0100

    add review entity

---
+++ src/main/java/jmr/springboot/flyway/entity/Review.java
@@ -0,0 +1,66 @@
+package jmr.springboot.flyway.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="reviews")
+public class Review
+{
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	@Column(name="id")
+	private Long id;
+	
+	@Column(name="manuscript")
+	private String manuscript;
+	
+	@Column(name="publishable")
+	private boolean publishable;
+	
+	public Review()
+	{
+		// empty constructor for JPA
+	}
+
+	public Review( String manuscript, boolean publishable )
+	{
+		this.manuscript = manuscript;
+		this.publishable = publishable;
+	}
+
+	public Long getId()
+	{
+		return id;
+	}
+
+	public void setId( Long id )
+	{
+		this.id = id;
+	}
+
+	public String getManuscript()
+	{
+		return manuscript;
+	}
+
+	public void setManuscript( String manuscript )
+	{
+		this.manuscript = manuscript;
+	}
+
+	public boolean isPublishable()
+	{
+		return publishable;
+	}
+
+	public void setPublishable( boolean publishable )
+	{
+		this.publishable = publishable;
+	}
+	
+}
